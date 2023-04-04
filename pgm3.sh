326253bccfb387a2fbaf5a397bed6bb98f53da6e
anithalaya <anithakoyikkal2@gmail.com>
2023-04-04 17:25:36 +0530

    fourth commit

---
+++ pgm3.sh
@@ -0,0 +1,19 @@
+echo "prime number "
+for ((i=1; i<=100; i++))
+do
+f=0
+j=2
+while [ $j -lt $i ]
+do
+if [ `expr $i % $j ` -eq 0 ]
+then
+f=1
+break
+fi
+j=`expr $j + 1`
+done
+if [ $f -eq 0 ]
+then
+echo "$i"
+fi
+done
